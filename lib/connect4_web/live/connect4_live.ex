dc29df904b7ca5ec9aba82dd864ccf4fe7a8af3a
Brooklin Myers <brooklinmeyers@Brooklins-MacBook-Pro.local>
2023-07-15 10:19:33 -0700

    draft partial ui

--- lib/connect4_web/live/connect4_live.ex
+++ lib/connect4_web/live/connect4_live.ex
@@ -2,20 +2,11 @@ defmodule Connect4Web.Connect4Live do
   use Connect4Web, :live_view
   use LiveViewNative.LiveView
 
-  @board [
-    [nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil]
-  ]
-
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, board: @board)}
+    {:ok, assign(socket, board_matrix: Connect4.board())}
   end
 
-  def render(assigns) do
+  def render(%{platform_id: :web} = assigns) do
     ~H"""
     Connect4
     """
@@ -23,7 +14,38 @@ defmodule Connect4Web.Connect4Live do
 
   def render(%{platform_id: :swiftui} = assigns) do
     ~SWIFTUI"""
-    <Text>Connect4 Mobile</Text>
+    <Text><%= board(@board_matrix) %></Text>
+    <Table>
+      <Group template={:columns}>
+        <TableColumn id="1">
+          <Text>Column1</Text>
+        </TableColumn>
+        <TableColumn id="2">
+          <Text>Column1</Text>
+        </TableColumn>
+        <TableColumn id="3">
+          <Text>Column1</Text>
+        </TableColumn>
+      </Group>
+      <Group template={:rows}>
+        <TableRow id="r1">
+          <Text>Row1</Text>
+        </TableRow>
+        <TableRow id="r2">
+          <Text>Row1</Text>
+        </TableRow>
+        <TableRow id="r3">
+          <Text>Row1</Text>
+        </TableRow>
+      </Group>
+    </Table>
+
+    """
+  end
+
+  def board(board_matrix) do
+    """
+    <Text>Test</Text>
     """
   end
 end
--- test/connect4_web/live/connect4_live_test.exs
+++ test/connect4_web/live/connect4_live_test.exs
@@ -1,8 +1,17 @@
 defmodule Connect4Web.Connect4LiveTest do
   use Connect4Web.ConnCase
 
+  alias Connect4Web.Connect4Live
+
   test "connected mount", %{conn: conn} do
     {:ok, view, html} = live(conn, "/")
     assert html =~ "Connect4"
+  end
+
+  test "board/1" do
+    board = Connect4.board()
+    assert Connect4Web.board(board) == """
+    <Text>Test</Text>
+    """
   end
 end
